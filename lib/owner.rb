f08945795baacc7612f7c470d7703fbb2a589b34
Candice Peters <candicelizabethpeters@gmail.com>
2019-08-31 04:05:28 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -68,6 +68,6 @@ class Owner
   end
   
  def list_pets
-    "I have #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
+    "I have #{@@pets[:dogs].length} dog(s), and #{@@pets[:cats].length} cat(s)."
   end
 end
